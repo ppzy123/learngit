bda0430f5bafcdd93d8f7109d88db937e33b9603
ppzy123 <1157928777@qq.com>
2021-03-07 20:49:55 +0800

    commit 1

--- 221801405/src/WordCount.java
+++ 221801405/src/WordCount.java
@@ -1,10 +1,13 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class WordCount {
     static public void main(String[] args) throws IOException {
+        String inPath="input.txt";
+        String outPath="output.txt";
         /*Scanner input=new Scanner(System.in);
-        String str=input.next();
-        System.out.println(str);*/
+        inPath=input.next();
+        outPath=input.next();*/
         Lib wc=new Lib();
         wc.SetPath("output.txt","input.txt");
         wc.Open();
